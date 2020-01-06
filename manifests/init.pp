f24679b73b51e4faf95ba39754bf4f51383c627b
Alexandru Raul <raul@deva-alpine-adventure.org>
2020-01-06 16:38:12 +0200

    First commit

---
+++ manifests/init.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include tomcat_instances
+class tomcat_instances {
+}
---
+++ manifests/instances.pp
@@ -0,0 +1,17 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include tomcat_instances::instances
+class tomcat_instances::instances {
+
+file { '/tmp/bla':
+        ensure => file,
+        owner => 'root',
+        group => 'root',
+         content => "This is a test file created using puppet.
+                            Puppet is really cool",
+    }
+
+}
